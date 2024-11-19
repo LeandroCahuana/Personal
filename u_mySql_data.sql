0df5943d0cffd82c054262acdfe745b035e4386e
LeandroCF2007 <leandro.cahuana@vallegrande.edu.pe>
2024-11-19 13:08:29 -0500

    u-mySql_data

--- u_mySql_data.sql
+++ u_mySql_data.sql
@@ -3,37 +3,39 @@
 -- SINTAXIS: INSERT INTO nombre_tabla (campos) VALUES (valores);
 -- EJEMPLOS:
 -- Registramos datos a la tabla "user"
-INSERT INTO user (name_user, lastname, email, cellphone) VALUES ("Carlos", "Perez", "carlos432@gmail.com", "985632741");
-
--- Registramos datos a la tabla "consultation"
-INSERT INTO consultation (user, consultation, agent, answer) VALUES (1, "consultaEjemplo", 1, "respuestaEjemplo");
+INSERT INTO user (name_user, lastname, email, cellphone, consultation) VALUES ("Carlos", "Perez", "carlos432@gmail.com", "985632741", "ConsultaEjemplo1");
 
 -- Registramos datos a la tabla "agent"
 INSERT INTO agent (name_agent, lastname, email, cellphone) VALUES ("juan", "fernandez", "juan768@gmail.com", "123985647");
+
+-- Registramos datos a la tabla "consultation"
+INSERT INTO consultation (user, agent, answer) VALUES (1, 1, "respuestaEjemplo");
 
 ## VISUALIZAR REGISTROS DE LAS TABLAS
 -- SINTAXIS: SELECT * FROM nombre_tabla;
 -- EJEMPLOS: 
 -- Visualizar los registros de la tabla "user"
-SELECT * FROM user;
+# SELECT * FROM user;
 
 -- Visualizar los registros de la tabla "consultation"
-SELECT * FROM consultation;
+# SELECT * FROM consultation;
 
 -- Visualizar los registros de la tabla "agent"
-SELECT * FROM agent;
+# SELECT * FROM agent;
 
 ## ACTUALIZAR LOS REGISTROS DE LAS TABLAS
 -- SINTAXIS: UPDATE nombre_tabla SET nombre_campo = nuevo_valor WHERE campo_buscado = valor_requerido;
 -- EJEMPLO:
 -- Actualizamos el número de celular de 'Juan' de la tabla 'agent'
-UPDATE agent
+/*UPDATE agent
 SET cellphone = 986532741
 WHERE id = 1;
+*/
 
 ## ELIMINACIÓN DE REGISTRO
 -- SINTAXIS: DELETE FROM nombre_tabla WHERE campo_buscado = valor_requerido;
 -- EJEMPLO:
 -- Eliminación del primer registro de la tabla 'agent';
-DELETE FROM agent
+/*DELETE FROM agent
 WHERE id = 1;
+*/
